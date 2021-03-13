aeebacc56022357bd63927abe20ec5a6e224707f
radoncreep <radoncreep@gmail.com>
2021-03-13 13:42:43 +0100

    fixed_duplicates

---
+++ app/components/GenreListComponent.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export const GenreListComponent = () => {
+
+    return (
+        <View style={styles.container}>
+            {/* genre component will be here */}
+            <Text>Goes here</Text>
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#fff'
+    },
+});
