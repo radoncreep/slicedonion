c0c91493cb499a55e53e1b9b8cc7ad39469a6527
radoncreep <radoncreep@gmail.com>
2021-04-30 15:28:30 +0100

    fixes

--- app/components/HorizontalTrays/SmallCardTray.js
+++ app/components/HorizontalTrays/SmallCardTray.js
@@ -37,7 +37,7 @@ const SmallCardTray = ({ data, navigation, towhere, heading }) => {
 const styles = StyleSheet.create({
     container: {
         marginHorizontal: 0,
-        height: 300,
+        height: 330,
         paddingLeft: 5, 
     },
     header: {
