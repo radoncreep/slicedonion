0d4de274c87ca974616b2fc623b1646db2bf506c
Victor Onofiok <radoncreep@gmail.com>
2022-01-01 13:18:41 +0100

    homepage_skeleton

--- app/components/Categories/PopularAnimeComponent.js
+++ app/components/Categories/PopularAnimeComponent.js
@@ -1,23 +1,28 @@
 import React, { useState, useEffect } from 'react';
-import { ActivityIndicator, StyleSheet } from 'react-native';
+import { StyleSheet, View, Text, ScrollView } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+import ContentLoader, { Rect, Circle } from 'react-content-loader/native';
+
 import { getAllPopularPage } from '../../api/getPopular';
-
 import { SmallCardTray } from '../HorizontalTrays';
-
 
 const PopularAnimeComponent = ({ navigation, param, towhere }) => {
     const [ popularShows, setPopularShows ] = useState([]);
-    const [ fetched, setFetched ] = useState(false);
+    const [ fetching, setFetching ] = useState(false);
+
+    const mapping = new Array(5).fill(0);
 
     const getPopularAnime = async () => {
         let mounted = true;
 
         try {
+            if (mounted)
+                setFetching(true);
+
             const { data, ok } = await getAllPopularPage(param);
             
             if (mounted && ok) {
-                console.log('popular loaded');
-                setFetched(true)
+                setFetching(false)
                 setPopularShows(data.list);
             };
         } catch (error) {
@@ -31,25 +36,75 @@ const PopularAnimeComponent = ({ navigation, param, towhere }) => {
         getPopularAnime();
     }, []);
 
+    function renderTraySkeleton() {
+        // remove heading prop
+        return (
+            <ScrollView
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={{
+                    paddingHorizontal: 0,
+                    flexDirection:  'row',
+                    marginTop: 10
+                }}
+            >
+                {mapping.map((item, index) => (
+                    <View
+                        key={`MAPPING-${index}`}
+                        style={{
+                            marginLeft: index > 0 ? 10 : 0,
+                        }}
+                    >
+                        <ContentLoader 
+                            speed={2}
+                            width={150}
+                            height={300}
+                            backgroundColor="#0f011f"
+                            foregroundColor="#330a21"
+                        >
+                            <Rect x="0" y="0" rx="4" ry="4" width="150" height="270" />
+                        </ContentLoader>
+                    </View>
+                ))}
+            </ScrollView>
+        )
+    }
+
+    function renderCardTray() {
+        return (
+            <SmallCardTray 
+                data={popularShows} 
+                navigation={navigation}
+                towhere={towhere}
+                heading="Popular Series"
+                fetching={fetching}
+                isFromHome={true}
+            />
+        )
+    }
+
     return (
-        <>
-            <ActivityIndicator visible={fetched} />
-            { fetched && (
-                <SmallCardTray 
-                    data={popularShows} 
-                    navigation={navigation}
-                    towhere={towhere}
-                    heading="Popular Series"
-                    fetched={fetched}
-                    isFromHome={true}
-                />  
-            )}
-        </>
+        <View style={styles.container}>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                <Text style={styles.heading}>Popular Series</Text>
+                <MaterialIcons style={{ marginTop: 3 }} name="chevron-right" size={26} color="white" />
+            </View>
+           {fetching ? renderTraySkeleton() : renderCardTray()}
+       </View>
     );
 };
 
 const styles = StyleSheet.create({
-    container: {},
+    container: {
+        marginHorizontal: 0,
+        height: 330,
+        paddingLeft: 5, 
+    },
+    heading: {
+        color: '#fff',
+        fontWeight: 'bold',
+        fontSize: 18,
+    },
     spinner: {
         height: 300,
         justifyContent: 'center',
--- app/components/Categories/Recents.js
+++ app/components/Categories/Recents.js
@@ -1,38 +1,114 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View, ScrollView, Text } from 'react-native';
+import ContentLoader, { Rect, Circle } from 'react-content-loader/native';
+import { MaterialIcons } from '@expo/vector-icons';
+
 import { getRecents } from '../../api/getRecentRelease';
 import { SmallCardTray } from '../HorizontalTrays';
 
 const Recents = ({ navigation, towhere }) => {
     const [ recents, setRecents ] = useState([]);
+    const [ fetching, setFetching ] = useState(false);
+    
+    const mapping = new Array(5).fill(0);
 
     const getRecentAnimeShows = async () => {
         try {
+            setFetching(true);
+           
             const response = await getRecents();
 
-            if (response.data && response.ok) setRecents(response.data.list);
+            if (response.data && response.ok) {
+                setFetching(false);
+                setRecents(response.data.list);
+            } else {
+                setFetching(true)
+            }
+
         } catch (error) {
             console.log(error);
         }
     }
 
     useEffect(() => {
-        getRecentAnimeShows();
+        let mounted = true;
+
+        if (mounted) {
+            getRecentAnimeShows();
+        }
+
+        return () =>  mounted = false;
     }, [])
 
+    function renderTraySkeleton() {
+        return (
+            <View style={styles.container}>
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                    <Text style={styles.heading}>Recent</Text>
+                    <MaterialIcons style={{ marginTop: 3 }} name="chevron-right" size={26} color="white" />
+                </View>
+                <ScrollView
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    contentContainerStyle={{
+                        paddingHorizontal: 0,
+                        flexDirection:  'row',
+                        marginTop: 10
+                    }}
+                >
+                    {mapping.map((item, index) => (
+                        <View
+                            key={`MAPPING-${index}`}
+                            style={{
+                                marginLeft: index > 0 ? 10 : 0,
+                            }}
+                        >
+                            <ContentLoader 
+                                speed={2}
+                                width={150}
+                                height={300}
+                                backgroundColor="#0f011f"
+                                foregroundColor="#330a21"
+                            >
+                                <Rect x="0" y="0" rx="4" ry="4" width="150" height="270" />
+                            </ContentLoader>
+                        </View>
+                    ))}
+                </ScrollView>
+            </View>
+        )
+    }
+
+    function renderCardTray() {
+        return (
+            <SmallCardTray 
+                data={recents} 
+                heading="Recents" 
+                towhere={towhere} 
+                navigation={navigation}
+                isFromHome={true}
+            />
+        )
+    }
+
     return (
-        <SmallCardTray 
-            data={recents} 
-            heading="Recents" 
-            towhere={towhere} 
-            navigation={navigation}
-            isFromHome={true}
-        />
+       <View>
+           {fetching ? renderTraySkeleton() : renderCardTray()}
+       </View>
     ) 
 };
 
 const styles = StyleSheet.create({
-    container: {},
+    container: {
+        marginHorizontal: 0,
+        height: 330,
+        paddingLeft: 5, 
+    },
+    heading: {
+        color: '#fff',
+        fontWeight: 'bold',
+        fontSize: 18,
+    }
 })
 
 export default Recents;
